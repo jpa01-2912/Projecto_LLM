29e59c716fd77763f874e9dd263d41ded0dcb7ff
jpa01-2912 <jpa01@iesemilidarder.com>
2026-05-07 00:03:20 +0200

    Ultima versión de BDD

--- SQL_Imports/07-05-2026.sql
+++ SQL_Imports/07-05-2026.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1:3307
--- Tiempo de generación: 07-05-2026 a las 00:01:19
+-- Tiempo de generación: 07-05-2026 a las 00:01:55
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
